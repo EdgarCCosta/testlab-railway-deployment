06d01570f1b6f45eaeccc9ce97ce9bc1d3aa49c8
EdgarCCosta <esantiagorodrigues@birt.es>
2025-12-28 13:56:19 +0100

    Final Dockerfile front&back12345678

--- testlab/BACKEND/routes/api.php
+++ testlab/BACKEND/routes/api.php
@@ -9,9 +9,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::options('{any}', function () {
-    return response()->noContent();
-})->where('any', '.*');
+
 
 Route::get('/ping', function () {return response()->json(['status' => 'ok']);});
 
