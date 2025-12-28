b9b4c5c7430ea3e00f11f6d9c2dae4c8f79d4943
EdgarCCosta <esantiagorodrigues@birt.es>
2025-12-28 04:16:55 +0100

    Final Dockerfile front&back12345

--- testlab/frontend/src/environments/environment.ts
+++ testlab/frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     // apiUrl: 'http://localhost/proyecto/testlab/testlab/backend/public/api'
-    apiUrl: 'http://localhost:8000/api'
+    apiUrl: 'https://testlab-backend-production.up.railway.app/api'
 };
