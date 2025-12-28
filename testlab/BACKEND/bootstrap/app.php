8f73cac23a538450334d8b68d1f74b1fa53fe165
EdgarCCosta <esantiagorodrigues@birt.es>
2025-12-28 14:03:03 +0100

    Final Dockerfile frontend&backend

--- testlab/BACKEND/bootstrap/app.php
+++ testlab/BACKEND/bootstrap/app.php
@@ -13,8 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
-        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
-
         // Sanctum (modo API o SPA)
         $middleware->statefulApi();
 
