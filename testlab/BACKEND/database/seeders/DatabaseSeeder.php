298141b1ae694f5b6ef943f57e6095d3b45e2ea1
EdgarCCosta <esantiagorodrigues@birt.es>
2025-12-28 03:20:27 +0100

    Final Dockerfile front1

--- testlab/BACKEND/database/seeders/DatabaseSeeder.php
+++ testlab/BACKEND/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+
+        DB::table('users')->truncate();
         // 1. Crear usuarios
         $admin = User::create([
             'name' => 'Admin UUser',
